267dcfeb24050fce4ae01bd019204f1d4de23cd6
Harshal Bhawe <78535979+HBhawe@users.noreply.github.com>
2022-05-30 10:54:58 +0200

    Delete 4_Merge.sh

--- 2_Scripts_Dataset/3_Paralogs/4_Merge.sh
+++
@@ -1,9 +0,0 @@
-#Merges all paralogs per gene
-
-for i in /proj/uppstore2018069/Harshal/2_Dataset/Paralogs/*.paralogs.fa
-do
-##f="${basename $i .paralogs.fa}"
-touch ${i%%.*}_empty.txt
-cat ${i%%.*}.fa ${i%%.*}_empty.txt >> $i
-rm ${i%%.*}_empty.txt
-done
